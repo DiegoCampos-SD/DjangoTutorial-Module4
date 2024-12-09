f9128073f5059e67e3b9e5fb7f649d79d73cac05
DieCamDu <dcamposd@ucenfotec.ac.cr>
2024-12-08 20:15:04 -0500

    Final commit security and comment class were created

--- blogDiego/models.py
+++ blogDiego/models.py
@@ -12,9 +12,23 @@ class Post(models.Model):
     def publish(self):
         self.publishedDate = timezone.now()
         self.save()
-#       return 'saved'
-        
     
+    def approved_comments(self):
+        return self.comments.filter(approvedComment=True)
+        
     def __str__(self):
         return self.title
     
+class Comment(models.Model):
+    post = models.ForeignKey('blogDiego.Post', on_delete=models.CASCADE, related_name='comments', db_column='post')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    createdDate = models.DateTimeField(default=timezone.now)
+    approvedComment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approvedComment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
